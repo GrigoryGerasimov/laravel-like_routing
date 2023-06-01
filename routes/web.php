f442534b6919ef6df894b7eb0b1e21b671e188b4
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-01 23:55:27 +0200

    route dispatcher upd, request uri and config route splitting implemented

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use GrigoryGerasimov\LaraLikeRouting\Core\Routing\Router;
 use GrigoryGerasimov\LaraLikeRouting\Http\Controllers\RouteControllers\RouteController;
 
-Router::get('/articles/1', [RouteController::class, 'index'])->name('articles.index')->middleware('jwt.auth');
+Router::get('/articles/{article}/test/{test}', [RouteController::class, 'index'])->name('articles.index')->middleware('jwt.auth');
