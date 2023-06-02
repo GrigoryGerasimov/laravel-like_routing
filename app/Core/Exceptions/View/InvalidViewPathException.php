23ac63fcb10475d8720c44782fcb4b43755d39d6
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-06-02 22:45:29 +0200

    view class added, article index view added, view exceptions added, route controller upd

---
+++ app/Core/Exceptions/View/InvalidViewPathException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GrigoryGerasimov\LaraLikeRouting\Core\Exceptions\View;
+
+class InvalidViewPathException extends ViewException
+{
+
+}
--- app/Http/Controllers/RouteControllers/RouteController.php
+++ app/Http/Controllers/RouteControllers/RouteController.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 namespace GrigoryGerasimov\LaraLikeRouting\Http\Controllers\RouteControllers;
 
 use GrigoryGerasimov\LaraLikeRouting\Http\Controllers\Controller;
+use GrigoryGerasimov\LaraLikeRouting\Core\View\View;
 
 class RouteController extends Controller
 {
     public function index(): void
     {
-        include(__DIR__.'/../../../../resources/views/home.php');
+        View::make('articles.index');
     }
 
     public function show(string $article, string $test): void
     {
+        View::make('home');
         print($article).PHP_EOL;
         print($test).PHP_EOL;
     }
